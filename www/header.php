0e2f734a142be1d06d9de9674529ec028830e87c
bartgro1 <bart@groothuysen.com>
2024-03-21 08:36:45 +0100

    header.php, index.php aangemaakt en styling toegevoegd

---
+++ www/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Flexbox Navigatiemenu</title>
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<nav>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">producten</a></li>
+    <li><a href="#">registeren</a></li>
+    <li><a href="#">categorieen</a></li>
+    <li><a href="#">menugang</a></li>
+  </ul>
+</nav>
+
+</body>
+</html>
+
