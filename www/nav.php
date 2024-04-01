fff90b6e9ea8a38fdf60aada963d4dd03a6964af
bartgro1 <bart@groothuysen.com>
2024-04-01 19:24:26 +0200

    reserveringen styling update

--- www/nav.php
+++ www/nav.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['user_id'])) {
             <li><a href="index.php" <?php if ($current_page == 'index.php') echo 'class="active"'; ?>>Home</a></li>
             <li><a href="#" <?php if ($current_page == 'menu.php') echo 'class="active"'; ?>>Menu</a></li>
             <!-- Reservering link -->
-            <?php if ($_SESSION['role'] !== 'admin' && $_SESSION['role'] !== 'directeur' && $_SESSION['role'] !== 'manager' && $_SESSION['role'] !== 'medewerker') : ?>
+           <?php if (isset($_SESSION['role']) && $_SESSION['role'] !== 'admin' && $_SESSION['role'] !== 'directeur' && $_SESSION['role'] !== 'manager' && $_SESSION['role'] !== 'medewerker') : ?>
                 <li><a href="reserveringen_create.php" <?php if ($current_page == 'reserveringen_create.php') echo 'class="active"'; ?>>Reservering</a></li>
             <?php endif; ?>
 
--- www/reserveringen_create.php
+++ www/reserveringen_create.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 // Check if the user is not logged in
@@ -34,31 +34,31 @@ $tafels = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body>
 <?php require 'nav.php' ?>
 <main>
-       <?php if (isset($_SESSION['role']) && ($_SESSION['role'] === 'admin' || $_SESSION['role'] === 'manager' || $_SESSION['role'] === 'medewerker')) { ?>
+    <div class="container">
+        <div class="reserveringen-container">
         <div class="account-pagina">
-        <?php } else { ?>
-        <div class="account-pagina2">
-         <?php } ?>
-
-        <div class="form-panel">    
+            <div class="reserveringen-panel">
+            <div class="form-panel"> 
             <h1>reserveren</h1> <!-- Form title -->
+            
             <hr class="separator"> <!-- Add horizontal line as a separator -->
-            <form action="reserveringen_create_process.php" method="POST">
+            <div class="white-space"></div>
+            <form action="reserveringen_create_process.php" method="POST">      
             <?php   
             if (isset($_SESSION['role']) && ($_SESSION['role'] === 'admin' || $_SESSION['role'] === 'manager' || $_SESSION['role'] === 'medewerker')) {
             ?>
-                <div class="input-groep">
-                    <label for="voornaam">Voornaam</label>
-                    <input type="text" id="voornaam" name="voornaam">
-                </div>
-                <div class="input-groep">
-                    <label for="tussenvoegsel">Tussenvoegsel</label>
-                    <input type="text" id="tussenvoegsel" name="tussenvoegsel">
-                </div>
-                <div class="input-groep">
-                    <label for="achternaam">Achternaam</label>
-                    <input type="text" id="achternaam" name="achternaam">
-                </div>
+                <div class="input-container">
+    <div class="input-groep">
+        <input type="text" id="voornaam" name="voornaam" placeholder="Voornaam">
+    </div>
+    <div class="input-groep">
+        <input type="text" id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel">
+    </div>
+    <div class="input-groep">
+        <input type="text" id="achternaam" name="achternaam" placeholder="achternaam">
+    </div>
+</div>
+
                 <div class="input-groep">
                     <label for="email">Email</label>
                     <input type="email" id="email" name="email">
@@ -120,7 +120,14 @@ $tafels = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </form>
         </div>
     </div>
+    </div> 
+</div>
+    </div>
+    </div>
+    </div>
 </main>
 <?php require 'footer.php' ?>
 </body>
 </html>
+
+
